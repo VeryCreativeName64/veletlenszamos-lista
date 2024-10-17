1c1270a65dd6289da6b472b47894df6745a3bce2
Marek Bendegúz Kristóf (Szf_N_2024_2026) <marekbendeguz@gmail.com>
2024-10-17 09:29:27 +0200

    2. és 3. feladat listával, megszámlálás

--- feladatok.py
+++ feladatok.py
@@ -16,3 +16,26 @@ def poz_szamok_szama(lista):
         i+=1
     
     return db
+
+def negativ_szamok_osszege(lista):
+    i:int=0
+    db:int=0
+    while(i<len(lista)):
+        if(lista[i]<0):
+            db+=1
+        i+=1
+    return db
+
+def ottel_oszthatok_atlaga(lista):
+    i:int=0
+    db:int=0
+    osszeg:int=0
+    while(i<len(lista)):
+        if(lista[i]%5==0):
+            db+=1
+            osszeg+=lista[i]
+        i+=1
+    atlag=osszeg/db
+        
+    return atlag
+        
--- main.py
+++ main.py
@@ -4,3 +4,11 @@ lista=[12, 21,56, 32, -5, -23, 35]
 print("1. feladat")
 db:int=feladatok.poz_szamok_szama(lista)
 print(f"A pozitív számok száma {db}")
+
+print("2. feladat")
+db:int=feladatok.negativ_szamok_osszege(lista)
+print(f"A negatív számok összege {db}")
+
+print("3. feladat")
+atlag:int=feladatok.ottel_oszthatok_atlaga(lista)
+print(f"Az öttel oszthatók átlaga {atlag}")
